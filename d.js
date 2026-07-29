175e34eff01aa6333f04664463106d56d1eb8a98
anuj paul <paulanuj27@gmail.com>
2026-07-29 21:30:58 +0600

    Array problem solve

---
+++ d.js
@@ -0,0 +1,24 @@
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+let j=0;
+   for (let i=0; i<nums.length; i++)
+   {
+       if (nums[j] !== nums[i])
+       {
+           j++;
+           nums[j] = nums[i]
+       }
+   }
+   return j+1
+};
+
+// for loops in js 
+for (let i =0; i<10; i++)
+{
+    console.log(i) // it's should output is 1-> 10 
+}
