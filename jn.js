6e0632cf0b3dbb8c385cb42f8f678f30b7d6a301
anuj paul <paulanuj27@gmail.com>
2026-07-31 08:49:27 +0600

    delete duplicate from the sorted array in two approach

--- jn.js
+++ jn.js
@@ -1,5 +1,20 @@
-let ar = [10,20,20,20,30,30,40,50]
+let ar = [5,5,7,7,9,10,10]
 
+let index = 0;
+
+for (let i=0; i<ar.length; i++)
+{
+    if (ar[i] !== ar[i+1])
+    {
+        ar[index] = ar[i]
+        index++;
+    }
+}
+ar.length =index
+
+
+// now i can solve with set data structure
 const sortedArr = [... new Set(ar)]
-console.log(sortedArr); // output is -> 10,20,30,40,50 
+console.log(sortedArr);
+
 
