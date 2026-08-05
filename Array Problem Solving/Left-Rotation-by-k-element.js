e1ed54ee50ab5b93ab36fe80a3c8061d5c8db560
anuj paul <paulanuj27@gmail.com>
2026-08-05 12:59:06 +0600

    Solve left rotation by k-th possition

---
+++ Array Problem Solving/Left-Rotation-by-k-element.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+let ar = [1,2,3,4,5,6]
+let k = prompt('Enter k: ');
+
+for (let i=0; i<k; i++)
+{
+    let temp = ar[0]
+    for (let j=0; j<ar.length-1; j++)
+    {
+        ar[j] = ar[j+1]
+    }
+    ar[ar.length-1] = temp 
+}
+
+console.log(ar);
