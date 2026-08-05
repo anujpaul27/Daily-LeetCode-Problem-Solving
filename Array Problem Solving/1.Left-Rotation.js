513a68b42d1a4ec9f54c7ea97ba5af3843145fa7
anuj paul <paulanuj27@gmail.com>
2026-08-05 10:57:07 +0600

    Array left rotation by one step

---
+++ Array Problem Solving/1.Left-Rotation.js
@@ -0,0 +1,27 @@
+/**
+ * What is array left rotation 
+ * arr = [1,2,3,4,5]
+ * output -> 2,3,4,5,1
+ */
+
+let ar= [1,2,3,4,5]
+const temp = ar[0]
+for (let i=0; i<ar.length-1; i++)
+{
+    ar[i] = ar[i+1]
+}
+ar[ar.length-1] = temp;
+
+console.log(ar);
+
+
+/*
+Approach is that-> 
+1,2,3,4,5
+2,2,3,4,5
+2,3,3,4,5
+2,3,4,4,5
+2,3,4,5,5
+2,3,4,5,1
+
+*/
