a70ac28aff39c2839bd510c13d88ead163f1e1c2
anuj paul <paulanuj27@gmail.com>
2026-08-07 10:45:14 +0600

    login update marge two sorted array

--- Array Problem Solving/4.Marge-Two-Sorted-Array.js
+++ Array Problem Solving/4.Marge-Two-Sorted-Array.js
@@ -3,18 +3,18 @@ const ar = [1,3,5,6]
 const ar1 = [2,4,7,8]
 const margeArr = [ar.length+ar1.length]
 
-let i=0,j=0;
-while (i < ar.length && j< ar1.length)
+let i=0,j=0,k=0;
+while (i < ar.length || j< ar1.length)
 {
     if (ar[i] < ar1[j])
     {
-        margeArr[i]= ar[i]
-        i++;
+        margeArr[k]= ar[i]
+        i++; k++;
     }
-    else 
+    if (!ar[i] < ar1[j]) 
     {
-        margeArr[i] = ar1[j]
-        j++
+        margeArr[k] = ar1[j]
+        j++, k++;
     }
 }
 
