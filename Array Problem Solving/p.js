394600add6d8ad1482a7621a3a590f40248fdbdb
anuj paul <paulanuj27@gmail.com>
2026-08-15 05:40:56 +0600

    Update some code

--- Array Problem Solving/p.js
+++ Array Problem Solving/p.js
@@ -13,4 +13,11 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 console.log(data);
 
 let decode = 100;
+let data;
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => data = json)
+console.log(data);
+
+let decode = 100;
 
