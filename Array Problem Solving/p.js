f42f45cdf523df869243e25895a46928aa33075e
anuj paul <paulanuj27@gmail.com>
2026-08-16 21:05:16 +0600

    Some comment hide

--- Array Problem Solving/p.js
+++ Array Problem Solving/p.js
@@ -20,4 +20,10 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 console.log(data);
 
 let decode = 100;
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => data = json)
+console.log(data);
+
+let decode = 100;
 
